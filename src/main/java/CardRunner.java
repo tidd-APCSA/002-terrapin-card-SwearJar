b938304aeba65eb6ab848282011fef191b6c6c42
Sam Schut <samuelschut23@gmail.com>
2020-10-16 23:51:44 +0000

    added paying, loading, and objects "billy" and "brian" as well as final system calls

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,43 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-        System.out.println("hey");
+        
+        TerrapinCard billy = new TerrapinCard(20);
+        TerrapinCard brian = new TerrapinCard(30);
+        System.out.println(billy);
+        System.out.println(brian);
+        billy.payGourmet();
+        brian.payEconomical();
+        System.out.println("Billy: "+billy);
+        System.out.println("Brian: " +brian);
+        brian.payGourmet();
+        billy.loadMoney(20);
+        System.out.println("Billy: "+billy);
+        System.out.println("Brian: " +brian);        
+        billy.payEconomical();
+        billy.payEconomical();
+        brian.loadMoney(50);
+        System.out.println("Billy: "+billy);
+        System.out.println("Brian: " +brian);    
 
+
+
+
+
+// Creates a TerrapinCard for Billy with initial balance of 20 dollars
+// Creates a TerrapinCard for Brian with initial balance of 30 dollars
+// Billy buys gourmet lunch
+// Brian buys economical lunch
+// cards are printed (both on their own row, starting with the name of the card owner)
+// Billy loads 20 dollars
+// Brian buys gourmet lunch
+// cards are printed (both on their own row, starting with the name of the card owner)
+// Billy buys economical lunch
+// Billy buys economical lunch
+// Brian loads 50 dollars
+// cards are printed (both on their own row, starting with the name of the card owner)
+
+
+        
     }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -6,4 +6,56 @@ public class TerrapinCard {
     this.balance = balance;
   }
 
+
+  public double getBalance(){
+    return balance;
+  }
+
+
+
+
+  public void payEconomical() {
+    double price = 2.5;
+    if(balance - price > 0){
+      balance -= price;
+      
+     }
+  }
+
+
+
+
+
+public void payGourmet() {
+  double price = 4.0;  
+  if(balance - price > 0){
+      balance -= price;
+  }
 }
+
+public void loadMoney(double amount) {
+double newBalance= balance +amount;
+double maxBalance = 150;
+
+if (amount<0){
+return;
+}
+if (newBalance > maxBalance){
+  balance = maxBalance;
+}
+else{
+  balance = newBalance;
+}
+}
+
+
+
+public String toString(){
+  return "This card has " + balance + " dollars loaded" ;
+}
+
+
+
+}
+
+
