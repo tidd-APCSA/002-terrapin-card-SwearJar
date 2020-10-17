d8cf61743528fedfce175b1fac7f94219a13423e
Sam Schut <samuelschut23@gmail.com>
2020-10-17 00:08:35 +0000

    Exercise 1

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -2,29 +2,8 @@ public class CardRunner {
     public static void main(String args[]){
         // ☕
         
-        TerrapinCard billy = new TerrapinCard(20);
-        TerrapinCard brian = new TerrapinCard(30);
-
-        billy.payGourmet();
-        brian.payEconomical();
-
-        System.out.println("Billy: "+billy);
-        System.out.println("Brian: " +brian);
-
-        brian.payGourmet();
-        billy.loadMoney(20);
-
-        System.out.println("Billy: "+billy);
-        System.out.println("Brian: " +brian); 
-
-        billy.payEconomical();
-        billy.payEconomical();
-
-        brian.loadMoney(50);
-        
-        System.out.println("Billy: "+billy);
-        System.out.println("Brian: " +brian);    
-
+    TerrapinCard card = new TerrapinCard(50);    
+    System.out.print(card);
 
 
 
